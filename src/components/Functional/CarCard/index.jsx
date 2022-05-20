1c48215a57b62107bf7a9ca77510f908c196a2a1
Rodrigo Poblete <rorro.poblete@gmail.com>
2022-05-19 23:22:47 -0400

    feat: feature add card component (#5)
    * Update dependencies
    
    Update the dev packages: babel-core babel-preset-env babel-preset react
    eslint-plugin-react and the project packages: mui-material
    
    * Update title site
    
    * Create CarCard component & upload car image
    
    The CardCar component is created from a card element of the mui framework. In addition,
    the img folder is created within assets where the image of a car is uploaded for the element's cover.
    
    * Add CarCard component to entry point
    
    * Change car image format from jpeg to webp
    
    Co-authored-by: Rodrigo Poblete <rodrigopoblete.dev@gmail.com>

---
+++ src/components/Functional/CarCard/index.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+import car1 from "../../../assets/img/car1.webp";
+
+export default function MultiActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={car1}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Hyundai Grand i10
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            ¿Buscas algún Hatchback nuevo? Ya disponible el Grand i10 año 2022,
+            0 kilómetro, equipamiento completo. Al contado o crédito.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button type="submit" fullWidth variant="contained">
+          Más Detalles
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
