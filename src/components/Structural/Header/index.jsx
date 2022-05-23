2128c62f1986765cd17bab32fae18c1355149203
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-05-23 16:00:03 -0400

    feat: add header component
    The hamburger menu is removed, a search bar and an image
    in svg format are included as a logo aligned to the left.

--- src/components/Structural/Header/index.jsx
+++ src/components/Structural/Header/index.jsx
@@ -1,31 +1,84 @@
-// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
-/** @jsx jsx */
-import { css, jsx } from "@emotion/react";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
-import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
+import logo from "../../../assets/img/logo_min.svg";
 
-const cssHeader = css({
-  backgroundColor: "white",
-  position: "fixed",
-  zIndex: "100",
-});
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
-const cssTopLine = (theme) =>
-  css({
-    backgroundColor: theme.palette.primary.light,
-  });
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
-export default function Header() {
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "15ch",
+      "&:focus": {
+        width: "23ch",
+      },
+    },
+  },
+}));
+
+const StyledLogo = styled("img")(() => ({
+  // TODO: Add breakpoint for logo to responsive view. For now, it's just a static image.
+  maxWidth: 100,
+}));
+
+export default function SearchAppBar() {
   return (
-    <Grid container align="center" css={cssHeader}>
-      <Grid container css={cssTopLine} alignContent="center">
-        <Container maxWidth="md" disableGutters>
-          <Grid item xs={12}>
-            Header
-          </Grid>
-        </Container>
-      </Grid>
-    </Grid>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <StyledLogo src={logo} alt="logo" />
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="EJ: Hyundai i10"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
