f87529a7eba0cf68df575e17ec8c1f2b3664920f
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-06-12 19:33:10 -0400

    refactor: import component CarCard name

--- src/components/Functional/Home/index.jsx
+++ src/components/Functional/Home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Grid from "@mui/material/Grid";
 
-import CardCard from "../CarCard/index";
+import CarCard from "../CarCard/index";
 
 import { dataCars } from "../../../../dataCars";
 
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Grid container spacing={2} justifyContent="center" maxWidth="md">
       {dataCars.map((car) => (
-        <CardCard key={car.id} carData={car} />
+        <CarCard key={car.id} carData={car} />
       ))}
     </Grid>
   );
