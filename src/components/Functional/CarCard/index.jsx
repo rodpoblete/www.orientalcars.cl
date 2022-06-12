3a4b33d8b05cc218ff5683c917c02f05dde1fa51
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-06-11 21:27:24 -0400

    refactor: move map function to parent component
    the map function and the grid container are moved to the parent
    Home component and a maxWidth is set to the container.
    Solve PR comments #25

--- src/components/Functional/CarCard/index.jsx
+++ src/components/Functional/CarCard/index.jsx
@@ -6,41 +6,37 @@ import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import Grid from "@mui/material/Grid";
 
-import { dataCars } from "../../../../dataCars";
-
-export default function MultiActionAreaCard() {
+export default function MultiActionAreaCard({
+  photo,
+  name,
+  year,
+  brand,
+  model,
+  price,
+  type,
+}) {
   return (
-    <Grid container spacing={2} justifyContent="center">
-      {dataCars.map((car) => (
-        <Grid item sx={4} sm={4} md={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="180"
-                image={car.photo}
-                alt={car.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                  {car.name}
-                </Typography>
-                <Typography>PRECIO: ${car.price}</Typography>
-                <Typography variant="body2" color="text.secondary">
-                  ¿Buscas algún {car.type} nuevo? Ya disponible el {car.brand}{" "}
-                  {car.model} año {car.year}, equipamiento completo. Al contado
-                  o crédito.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button type="submit" fullWidth variant="contained">
-                Más Detalles
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
+    <Grid item sx={4} sm={4} md={3}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia component="img" height="180" image={photo} alt={name} />
+          <CardContent>
+            <Typography gutterBottom variant="h7" component="div">
+              {name}
+            </Typography>
+            <Typography>PRECIO: ${price}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              ¿Buscas algún {type} nuevo? Ya disponible el {brand} {model} año{" "}
+              {year}, equipamiento completo. Al contado o crédito.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button type="submit" fullWidth variant="contained">
+            Más Detalles
+          </Button>
+        </CardActions>
+      </Card>
     </Grid>
   );
 }
--- src/components/Functional/Home/index.jsx
+++ src/components/Functional/Home/index.jsx
@@ -4,17 +4,28 @@ import Grid from "@mui/material/Grid";
 
 import CardCard from "../CarCard/index";
 
+import { dataCars } from "../../../../dataCars";
+
 export default function Home() {
   return (
-    <>
-      <Grid container alignItems="center">
-        <Grid item xs={12} align="center"></Grid>
-      </Grid>
-      <Grid container alignItems="center">
-        <Grid item xs={12} align="center">
-          <CardCard />
-        </Grid>
-      </Grid>
-    </>
+    // <Grid container alignItems="center">
+    //   <Grid item xs={12} align="center">
+    //     <CardCard />
+    //   </Grid>
+    // </Grid>
+    <Grid container spacing={2} justifyContent="center" maxWidth="md">
+      {dataCars.map((car) => (
+        <CardCard
+          key={car.id}
+          photo={car.photo}
+          name={car.name}
+          year={car.year}
+          brand={car.brand}
+          model={car.model}
+          price={car.price}
+          type={car.type}
+        />
+      ))}
+    </Grid>
   );
 }
