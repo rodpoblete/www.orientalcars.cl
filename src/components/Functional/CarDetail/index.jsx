63e6c22d51f13dbbdcd095ea2dd9d44166f10f4e
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-07-19 01:29:06 -0400

    chore: layout component

--- src/components/Functional/CarDetail/index.jsx
+++ src/components/Functional/CarDetail/index.jsx
@@ -6,12 +6,39 @@ import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions, Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
 
+import { dataCars } from "../../../../dataCars";
+
+import { moneyFormat } from "helpers/numbers";
+
 export default function carDetail() {
   return (
-    <Grid sx={{ display: "flex" }}>
+    <Grid>
       <Card>
         <CardActionArea>
-          <Typography>hola mundo</Typography>
+          <CardMedia
+            component="img"
+            height="180"
+            image={dataCars[0].photo}
+            alt={dataCars[0].name}
+          />
+          <CardContent>
+            <Typography
+              sx={{
+                color: "text.primary",
+                fontSize: 24,
+                fontWeight: "medium",
+              }}
+            >
+              {dataCars[0].name}
+            </Typography>
+            <Typography>Precio: {moneyFormat(dataCars[0].price)}</Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              similique error, perspiciatis est iusto, eum maxime voluptatem
+              exercitationem quibusdam incidunt officia iure voluptatum natus
+              quos recusandae tenetur sunt quo dicta.
+            </Typography>
+          </CardContent>
         </CardActionArea>
       </Card>
     </Grid>
