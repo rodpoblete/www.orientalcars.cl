389cd44db030e9de517e95a510e344d551136aa3
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-07-22 20:53:51 -0400

    feat: Add extended car card with more information
    The extended card of the vehicle with more details of the home view is added.
    It is an intermediate view between the original card and the full detail version.

--- src/components/Functional/CarDetail/index.jsx
+++ src/components/Functional/CarDetail/index.jsx
@@ -1,46 +1,54 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions, Box } from "@mui/material";
+import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
-
-import { dataCars } from "../../../../dataCars";
 
 import { moneyFormat } from "helpers/numbers";
 
+import { dataCars } from "../../../../dataCars";
+
 export default function carDetail() {
   return (
-    <Grid>
-      <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="180"
-            image={dataCars[0].photo}
-            alt={dataCars[0].name}
-          />
-          <CardContent>
-            <Typography
-              sx={{
-                color: "text.primary",
-                fontSize: 24,
-                fontWeight: "medium",
-              }}
-            >
-              {dataCars[0].name}
-            </Typography>
-            <Typography>Precio: {moneyFormat(dataCars[0].price)}</Typography>
-            <Typography>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-              similique error, perspiciatis est iusto, eum maxime voluptatem
-              exercitationem quibusdam incidunt officia iure voluptatum natus
-              quos recusandae tenetur sunt quo dicta.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
+    <Container
+      sx={{
+        maxWidth: "md",
+        display: "grid",
+        gap: 1,
+        gridTemplateColumns: "1fr 3fr",
+        border: "1px solid #e0e0e0",
+      }}
+    >
+      <Grid>
+        <Box
+          component="img"
+          sx={{
+            height: 160,
+            maxWidth: 200,
+            overflow: "hidden",
+          }}
+          src={dataCars[0].photo}
+          alt={dataCars[0].name}
+        />
+      </Grid>
+      <Grid sx={{ display: "grid", gap: 1, alignItems: "start" }}>
+        <Typography
+          sx={{
+            color: "primary.main",
+            fontSize: 22,
+            fontWeight: "medium",
+          }}
+        >
+          {dataCars[0].name}
+        </Typography>
+        <Typography>Precio: {moneyFormat(dataCars[0].price)}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          similique error, perspiciatis est iusto, eum maxime voluptatem
+          exercitationem quibusdam incidunt officia iure voluptatum natus quos
+          recusandae tenetur sunt quo dicta.
+        </Typography>
+      </Grid>
+    </Container>
   );
 }
