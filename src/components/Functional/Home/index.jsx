4663222dd1c3323c9a81a81f61822d76f337aba6
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-09-04 22:21:35 -0400

    wip: implement CarDetail compoment carousel with swiper

--- src/components/Functional/Home/index.jsx
+++ src/components/Functional/Home/index.jsx
@@ -2,6 +2,15 @@ import React from "react";
 
 import Grid from "@mui/material/Grid";
 
+import { Navigation, Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import CarCard from "../CarCard/index";
 
 import { dataCars } from "../../../../dataCars";
@@ -9,9 +18,24 @@ import { dataCars } from "../../../../dataCars";
 export default function Home() {
   return (
     <Grid container spacing={2} justifyContent="center" maxWidth="md">
-      {dataCars.map((car) => (
-        <CarCard key={car.id} carData={car} />
-      ))}
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={3}
+        slidesPerGroup={3}
+        loop
+        loopFillGroupWithBlank
+        pagination={{
+          clickable: true,
+        }}
+        navigation
+        modules={[Pagination, Navigation]}
+      >
+        {dataCars.map((car) => (
+          <SwiperSlide key={car.id}>
+            <CarCard key={car.id} carData={car} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Grid>
   );
 }
