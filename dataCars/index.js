9acc7e61fe301f302b0ae6ffedf182172c18ee59
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-10-11 00:24:48 -0300

    feat: import new cars images & add description key value

--- dataCars/index.js
+++ dataCars/index.js
@@ -1,15 +1,89 @@
-const chevroletOnix = require("../src/assets/img/cars/chevrolet-onix-2022.webp");
-const hyundaiGrand22 = require("../src/assets/img/cars/hyundai-grand-1-10-2022.webp");
-const hyundaiGrand19 = require("../src/assets/img/cars/hyundai-grandi10-2019.webp");
-const susukiAlto = require("../src/assets/img/cars/susuki-alto-2022.webp");
-const susukiBaleno = require("../src/assets/img/cars/susuki-baleno-gls-2022.webp");
-const susukiSpresso = require("../src/assets/img/cars/susuki-spresso-glx-2021.webp");
-const toyotaRaize = require("../src/assets/img/cars/toyota-raize-2022.webp");
+const cheryTigo2PassengerSeatLeft = require("../src/assets/img/cars/cheryTigo2/chery2_passenger_seat_left.webp");
+const cheryTigo2Back = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_back.webp");
+const cheryTigo2CentralPanel = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_central_panel.webp");
+const cheryTigo2CopilotSeat = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_copilot_seat_right.webp");
+const cheryTigo2Cover = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_cover.webp");
+const cheryTigo2Extra = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_extra.webp");
+const cheryTigo2LateralBackLeft = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_lateral_back_left.webp");
+const cheryTigoLateralBackRight = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_lateral_back_right.webp");
+const cheryTigo2LateralFrontLeft = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_lateral_front_left.webp");
+const cheryTigoLateralFrontRight = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_lateral_front_right.webp");
+const cheryTigo2PassengerSeatRight = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_passenger_seat_right.webp");
+const cheryTigo2PilotSeat = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_pilot_seat_left.webp");
+const cheryTigo2SteeringWheel = require("../src/assets/img/cars/cheryTigo2/chery_tigo2_steering_wheel.webp");
+
+const kiaSportageBack = require("../src/assets/img/cars/kiaSportage/kia_sportage_back.webp");
+const kiaSportageCentralPanel = require("../src/assets/img/cars/kiaSportage/kia_sportage_central_panel.webp");
+const kiaSportageCopilotSeat = require("../src/assets/img/cars/kiaSportage/kia_sportage_copilot_seat_right.webp");
+const kiaSportageCover = require("../src/assets/img/cars/kiaSportage/kia_sportage_cover.webp");
+const kiaSportageExtra = require("../src/assets/img/cars/kiaSportage/kia_sportage_extra.webp");
+const kiaSportageLateralBackLeft = require("../src/assets/img/cars/kiaSportage/kia_sportage_lateral_back_left.webp");
+const kiaSportageLateralBackRight = require("../src/assets/img/cars/kiaSportage/kia_sportage_lateral_back_right.webp");
+const kiaSportageLateralFrontLeft = require("../src/assets/img/cars/kiaSportage/kia_sportage_lateral_front_left.webp");
+const kiaSportageLateralFrontRight = require("../src/assets/img/cars/kiaSportage/kia_sportage_lateral_front_right.webp");
+const kiaSportagePassengerSeatLeft = require("../src/assets/img/cars/kiaSportage/kia_sportage_passenger_seat_left.webp");
+const kiaSportagePassengerSeatRight = require("../src/assets/img/cars/kiaSportage/kia_sportage_passenger_seat_right.webp");
+const kiaSportagePilotSeat = require("../src/assets/img/cars/kiaSportage/kia_sportage_pilot_seat_left.webp");
+const kiaSportageSteeringWheel = require("../src/assets/img/cars/kiaSportage/kia_sportage_steering_wheel.webp");
+
+const nissanVersaBack = require("../src/assets/img/cars/nissanVersa/nissan_versa_back.webp");
+const nissanVersaCentralPanel = require("../src/assets/img/cars/nissanVersa/nissan_versa_central_panel.webp");
+const nissanVersaCopilotSeat = require("../src/assets/img/cars/nissanVersa/nissan_versa_copilot_seat_right.webp");
+const nissanVersaCover = require("../src/assets/img/cars/nissanVersa/nissan_versa_cover.webp");
+const nissanVersaExtra = require("../src/assets/img/cars/nissanVersa/nissan_versa_extra.webp");
+const nissanVersaLateralBackLeft = require("../src/assets/img/cars/nissanVersa/nissan_versa_lateral_back_left.webp");
+const nissanVersaLateralFrontLeft = require("../src/assets/img/cars/nissanVersa/nissan_versa_lateral_front_left.webp");
+const nissanVersaPassengerSeatLeft = require("../src/assets/img/cars/nissanVersa/nissan_versa_passenger_seat_left.webp");
+const nissanVersaPassengerSeatRight = require("../src/assets/img/cars/nissanVersa/nissan_versa_passenger_seat_right.webp");
+const nissanVersaPilotSeat = require("../src/assets/img/cars/nissanVersa/nissan_versa_pilot_seat_left.webp");
+const nissanVersaSteeringWheel = require("../src/assets/img/cars/nissanVersa/nissan_versa_steering_wheel.webp");
+const nissanVersaTrunk = require("../src/assets/img/cars/nissanVersa/nissan_versa_trunk.webp");
+
+const renaultKoleosBack = require("../src/assets/img/cars/renaultKoleos/renault_koleos_back.webp");
+const renaultKoleosCentralPanel = require("../src/assets/img/cars/renaultKoleos/renault_koleos_central_panel.webp");
+const renaultKoleosCopilotSeat = require("../src/assets/img/cars/renaultKoleos/renault_koleos_copilot_seat_right.webp");
+const renaultKoleosCover = require("../src/assets/img/cars/renaultKoleos/renault_koleos_cover.webp");
+const renaultKoleosLateralBackLeft = require("../src/assets/img/cars/renaultKoleos/renault_koleos_lateral_back_left.webp");
+const renaultKoleosLateralBackRight = require("../src/assets/img/cars/renaultKoleos/renault_koleos_lateral_back_right.webp");
+const renaultKoleosLateralFrontLeft = require("../src/assets/img/cars/renaultKoleos/renault_koleos_lateral_front_left.webp");
+const renaultKoleosPanoramicInside = require("../src/assets/img/cars/renaultKoleos/renault_koleos_panoramic_inside.webp");
+const renaultKoleosPassengerSeatLeft = require("../src/assets/img/cars/renaultKoleos/renault_koleos_passenger_seat_left.webp");
+const renaultKoleosPassengerSeatRight = require("../src/assets/img/cars/renaultKoleos/renault_koleos_passenger_seat_right.webp");
+const renaultKoleosPilotSeat = require("../src/assets/img/cars/renaultKoleos/renault_koleos_pilot_seat_left.webp");
+const renaultKoleosRoof = require("../src/assets/img/cars/renaultKoleos/renault_koleos_roof.webp");
+const renaultKoleosSteeringWheel = require("../src/assets/img/cars/renaultKoleos/renault_koleos_steering_wheel.webp");
+const renaultKoleosTrunk = require("../src/assets/img/cars/renaultKoleos/renault_koleos_trunk.webp");
+
+const subaruImprezaBack = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_back.webp");
+const subaruImprezaCentralPanel = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_central_panel.webp");
+const subaruImprezaCopilotSeat = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_copilot_seat_right.webp");
+const subaruImprezaCover = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_cover.webp");
+const subaruImprezaLateralBackLeft = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_lateral_back_left.webp");
+const subaruImprezaLateralFrontLeft = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_lateral_front_left.webp");
+const subaruImprezaLateralFrontRight = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_lateral_front_right.webp");
+const subaruImprezaPassengerSeatLeft = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_passenger_seat_left.webp");
+const subaruImprezaPassengerSeatRight = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_passenger_seat_right.webp");
+const subaruImprezaPilotSeat = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_pilot_seat_left.webp");
+const subaruImprezaSteeringWheel = require("../src/assets/img/cars/subaruImpreza/subaru_impreza_steering_wheel.webp");
 
 exports.dataCars = [
   {
     id: 1,
-    photos: [hyundaiGrand22],
+    photos: [
+      cheryTigo2Cover,
+      cheryTigo2PassengerSeatLeft,
+      cheryTigo2Back,
+      cheryTigo2CentralPanel,
+      cheryTigo2CopilotSeat,
+      cheryTigo2Extra,
+      cheryTigo2LateralBackLeft,
+      cheryTigoLateralBackRight,
+      cheryTigo2LateralFrontLeft,
+      cheryTigoLateralFrontRight,
+      cheryTigo2PassengerSeatRight,
+      cheryTigo2PilotSeat,
+      cheryTigo2SteeringWheel,
+    ],
     name: "Chery Tiggo 2",
     year: 2019,
     brand: "Chery",
@@ -24,28 +98,25 @@ exports.dataCars = [
     doors: 5,
     type: "suv",
     keys: 2,
+    description:
+      "¿Quieres un Suv económico y rendidor?, El mini SUV Chery Tiggo 2 urbano, de diseño deportivo y excelente rendimiento, va contigo a todas partes. Con un motor 1.5L de 110 HP, 6 velocidades y un consumo de combustible de 7.5 km/l, es el vehículo ideal para moverte por la ciudad. Además, cuenta con un sistema de frenos ABS, EBD y EBA, que te brindan seguridad y confianza en la carretera. ¡No esperes más y conoce el Chery Tiggo 2!",
   },
   {
     id: 2,
-    photos: [susukiSpresso],
-    name: "Hyundai Accent GL 2018",
-    year: 2018,
-    brand: "Hyundai",
-    model: "Accent",
-    version: "RB GL AC 6MT",
-    owners: 2,
-    price: 10990000,
-    mileage: 47000,
-    fuel: "Bencina",
-    transmission: "Manual",
-    seats: 5,
-    doors: 4,
-    type: "Sedan",
-    keys: 2,
-  },
-  {
-    id: 3,
-    photos: [susukiAlto],
+    photos: [
+      nissanVersaCover,
+      nissanVersaBack,
+      nissanVersaCentralPanel,
+      nissanVersaCopilotSeat,
+      nissanVersaExtra,
+      nissanVersaLateralBackLeft,
+      nissanVersaLateralFrontLeft,
+      nissanVersaPassengerSeatLeft,
+      nissanVersaPassengerSeatRight,
+      nissanVersaPilotSeat,
+      nissanVersaSteeringWheel,
+      nissanVersaTrunk,
+    ],
     name: "Nissan Versa Sense 1.6 MT 2021",
     year: 2021,
     brand: "Nissan",
@@ -60,10 +131,25 @@ exports.dataCars = [
     doors: 4,
     type: "Sedan",
     keys: 2,
+    description: "",
   },
   {
-    id: 4,
-    photos: [hyundaiGrand19],
+    id: 3,
+    photos: [
+      kiaSportageCover,
+      kiaSportageBack,
+      kiaSportageCentralPanel,
+      kiaSportageCopilotSeat,
+      kiaSportageExtra,
+      kiaSportageLateralBackLeft,
+      kiaSportageLateralBackRight,
+      kiaSportageLateralFrontLeft,
+      kiaSportageLateralFrontRight,
+      kiaSportagePassengerSeatLeft,
+      kiaSportagePassengerSeatRight,
+      kiaSportagePilotSeat,
+      kiaSportageSteeringWheel,
+    ],
     name: "Kia Sportage QL 2.0 AUT 2021",
     year: 2021,
     brand: "Kia",
@@ -78,15 +164,28 @@ exports.dataCars = [
     doors: 5,
     type: "suv",
     keys: 2,
+    description: "",
   },
   {
-    id: 5,
-    photos: [susukiBaleno],
-    name: "Toyota Yaris GLI E CVT 2023",
-    year: 2023,
-    brand: "Toyota",
-    model: "Yaris",
-    version: "GLI E 1.5 CVT",
+    id: 4,
+    photos: [
+      subaruImprezaCover,
+      subaruImprezaBack,
+      subaruImprezaCentralPanel,
+      subaruImprezaCopilotSeat,
+      subaruImprezaLateralBackLeft,
+      subaruImprezaLateralFrontLeft,
+      subaruImprezaLateralFrontRight,
+      subaruImprezaPassengerSeatLeft,
+      subaruImprezaPassengerSeatRight,
+      subaruImprezaPilotSeat,
+      subaruImprezaSteeringWheel,
+    ],
+    name: "Subaru Impreza",
+    year: 2019,
+    brand: "Subaru",
+    model: "Impreza",
+    version: "2.0L",
     owners: 1,
     price: 14990000,
     mileage: 0,
@@ -96,10 +195,26 @@ exports.dataCars = [
     doors: 4,
     type: "Sedan",
     keys: 2,
+    description: "",
   },
   {
-    id: 6,
-    photos: [toyotaRaize],
+    id: 5,
+    photos: [
+      renaultKoleosCover,
+      renaultKoleosBack,
+      renaultKoleosCentralPanel,
+      renaultKoleosCopilotSeat,
+      renaultKoleosLateralBackLeft,
+      renaultKoleosLateralBackRight,
+      renaultKoleosLateralFrontLeft,
+      renaultKoleosPanoramicInside,
+      renaultKoleosPassengerSeatLeft,
+      renaultKoleosPassengerSeatRight,
+      renaultKoleosPilotSeat,
+      renaultKoleosRoof,
+      renaultKoleosSteeringWheel,
+      renaultKoleosTrunk,
+    ],
     name: "Renault Koleos 2.5 AT 2014",
     year: 2014,
     brand: "Renault",
@@ -114,41 +229,6 @@ exports.dataCars = [
     doors: 5,
     type: "Suv",
     keys: 2,
-  },
-  {
-    id: 7,
-    photos: [chevroletOnix],
-    name: "Toyota Yaris GLI E MT 2022",
-    year: 2022,
-    brand: "Toyota",
-    model: "Yaris",
-    version: "GLI E 1.5 MT",
-    owners: 1,
-    price: 14490000,
-    mileage: 7000,
-    fuel: "Bencina",
-    transmission: "Manual",
-    seats: 5,
-    doors: 4,
-    type: "Sedan",
-    keys: 2,
-  },
-  {
-    id: 8,
-    photos: [chevroletOnix],
-    name: "Toyota Yaris Sport 1.5 AT",
-    year: 2014,
-    brand: "Toyota",
-    model: "Yaris",
-    version: "SPORT 1.5 AT",
-    owners: 3,
-    price: 9890000,
-    mileage: 81000,
-    fuel: "Bencina",
-    transmission: "Automático",
-    seats: 5,
-    doors: 5,
-    type: "hb",
-    keys: 1,
+    description: "",
   },
 ];
