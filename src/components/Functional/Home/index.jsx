4c55bb098e417b1a45608926392725c77f10f681
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-10-22 22:21:30 -0300

    fix: carousel of card cars issue #42
    Closed #42

--- src/components/Functional/Home/index.jsx
+++ src/components/Functional/Home/index.jsx
@@ -2,16 +2,42 @@ import React from "react";
 
 import Grid from "@mui/material/Grid";
 
+import { Autoplay, Navigation } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
 import CarCard from "../CarCard/index";
 
 import { dataCars } from "../../../../dataCars";
 
 export default function Home() {
   return (
-    <Grid container spacing={2} justifyContent="center" maxWidth="md">
-      {dataCars.map((car) => (
-        <CarCard key={car.id} carData={car} />
-      ))}
+    <Grid container maxWidth="md" sx={{ paddingLeft: 1, paddingRight: 1 }}>
+      <Swiper
+        breakpoints={{
+          320: { slidesPerView: 2, spaceBetween: 10 },
+          480: { slidesPerView: 3, spaceBetween: 15 },
+          768: { slidesPerView: 4, spaceBetween: 20 },
+          1024: { slidesPerView: 4, spaceBetween: 25 },
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop
+        navigation
+        modules={[Autoplay, Navigation]}
+      >
+        {dataCars.map((car) => (
+          <SwiperSlide key={car.id}>
+            <CarCard key={car.id} carData={car} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Grid>
   );
 }
