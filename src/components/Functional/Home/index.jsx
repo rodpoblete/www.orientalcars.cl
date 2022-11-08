62a2fe5e548019e0703255542af501f8ea2e9bca
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-11-08 13:33:19 -0300

    feat: add SalesSection component to Home

--- src/components/Functional/Home/index.jsx
+++ src/components/Functional/Home/index.jsx
@@ -4,16 +4,20 @@ import Grid from "@mui/material/Grid";
 
 import SliderPrincipal from "../SliderPrincipal/index";
 import SliderCarCard from "../SliderCarCard/index";
+import SalesSection from "../SalesSection/index";
 
 export default function Home() {
   return (
-    <Grid container maxWidth="md" sx={{ paddingLeft: 1, paddingRight: 1 }}>
-      <Grid item xs={12}>
-        <SliderPrincipal />
+    <>
+      <Grid container maxWidth="md" sx={{ paddingLeft: 1, paddingRight: 1 }}>
+        <Grid item xs={12}>
+          <SliderPrincipal />
+        </Grid>
+        <Grid item xs={12}>
+          <SliderCarCard />
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <SliderCarCard />
-      </Grid>
-    </Grid>
+      <SalesSection />
+    </>
   );
 }
