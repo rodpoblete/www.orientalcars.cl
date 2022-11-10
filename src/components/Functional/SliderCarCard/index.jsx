3b7a0a498cac7bd8a3985e2b1c2716a9e9045036
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2022-11-10 14:03:00 -0300

    feat: add text & button home car galery

--- src/components/Functional/SliderCarCard/index.jsx
+++ src/components/Functional/SliderCarCard/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 import { Autoplay, Navigation } from "swiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 // Import Swiper styles
 import "swiper/css";
@@ -13,26 +16,44 @@ import { dataCars } from "../../../../dataCars";
 
 export default function SliderCarCard() {
   return (
-    <Swiper
-      breakpoints={{
-        320: { slidesPerView: 2, spaceBetween: 10 },
-        480: { slidesPerView: 3, spaceBetween: 15 },
-        768: { slidesPerView: 4, spaceBetween: 20 },
-        1024: { slidesPerView: 4, spaceBetween: 25 },
-      }}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      loop
-      navigation
-      modules={[Autoplay, Navigation]}
-    >
-      {dataCars.map((car) => (
-        <SwiperSlide key={car.id}>
-          <CarCard key={car.id} carData={car} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Typography
+        align="center"
+        color="primary"
+        sx={{ fontSize: "1.7rem", fontWeight: "bold" }}
+      >
+        Nuestros Autos
+      </Typography>
+      <Typography align="center" sx={{ paddingBottom: "15px" }}>
+        Encuentra el auto que estás buscando entre los distintos modelos que
+        tenemos disponibles para ti.
+      </Typography>
+      <Swiper
+        breakpoints={{
+          320: { slidesPerView: 2, spaceBetween: 10 },
+          480: { slidesPerView: 3, spaceBetween: 15 },
+          768: { slidesPerView: 4, spaceBetween: 20 },
+          1024: { slidesPerView: 4, spaceBetween: 25 },
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop
+        navigation
+        modules={[Autoplay, Navigation]}
+      >
+        {dataCars.map((car) => (
+          <SwiperSlide key={car.id}>
+            <CarCard key={car.id} carData={car} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Grid mt={2} mb={2} sx={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="contained" color="secondary">
+          Modelos Disponibles
+        </Button>
+      </Grid>
+    </>
   );
 }
