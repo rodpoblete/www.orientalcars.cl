ffce243ddbba57b85cd931fbb74c32e7f9a57648
Rodrigo Poblete <rorro.poblete@gmail.com>
2022-11-12 11:24:12 -0300

    feat: home sales section (#89)
    * feat: add wallpaper image sales home section
    
    * feat: create SalesSection component
    
    * feat: add SalesSection component to Home
    
    * chore: component SalesSection is wrapped in grid item
    
    * chore: add layout properties parent Box element
    
    * refactor: move padding props to CarDetail component
    
    * feat: add content to SalesSection component
    
    * chore: delete sx empty property in grid item
    
    * chore: disable button & delete fontSize prop
    
    Co-authored-by: Rodrigo Poblete <rodrigopoblete.dev@gmail.com>

---
+++ src/components/Functional/SalesSection/index.jsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const salesWallpaper = require("img/backgrounds/salesWallpaper.webp");
+
+export default function SalesSection() {
+  return (
+    <Box
+      sx={{
+        height: "300px",
+        display: "flex",
+        flexDirection: "column",
+        position: "relative",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        component="img"
+        src={salesWallpaper}
+        sx={{
+          width: "100vw",
+          height: "300px",
+          objectFit: "cover",
+          objectPosition: "center center",
+          filter: "brightness(0.5)",
+          position: "absolute",
+        }}
+      />
+      <Typography
+        variant="h3"
+        align="center"
+        mb={1}
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          color: "white",
+          fontWeight: "bold",
+          fontSize: {
+            xs: 26,
+            sm: 40,
+          },
+        }}
+      >
+        ¿Quieres vender tu vehículo?
+      </Typography>
+      <Typography
+        align="center"
+        variant="h5"
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          color: "white",
+          fontSize: {
+            xs: 20,
+            sm: 24,
+          },
+        }}
+      >
+        Te ayudamos con la venta de tu auto
+      </Typography>
+      <Typography
+        variant="h7"
+        align="center"
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          color: "white",
+          display: "inline",
+          fontSize: {
+            xs: 13,
+            sm: 16,
+          },
+        }}
+      >
+        Podemos ofrecerte distintas alternativas, como la venta directa
+        (nosotros te lo compramos)
+      </Typography>
+      <Typography
+        variant="h7"
+        align="center"
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          color: "white",
+          display: "inline",
+          fontSize: {
+            xs: 13,
+            sm: 16,
+          },
+        }}
+      >
+        o la consignación (te ayudamos a venderlo al mejor precio)
+      </Typography>
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ visibility: "hidden" }}
+      >
+        Más Información
+      </Button>
+    </Box>
+  );
+}
