37126ff5b6dbd7dea3e5d76fd685cc42c3116239
Rodrigo Poblete <rorro.poblete@gmail.com>
2022-11-25 19:04:35 -0300

    feat: actualiza catalogo de autos (#113)
    * feat: add new models cars photos
    
    * chore: add sold banner cars not available
    
    * feat: add new models cars data
    
    * feat: add new model suzuki swift gl data
    
    * feat: add suzuki swift gl sport images
    
    * chore: replace images with embedded banner
    
    * feat: add conditional rendering cars sold
    
    * chore: minimize the weight of car images
    
    * chore: adjust images sizes
    
    Co-authored-by: Rodrigo Poblete <rodrigopoblete.dev@gmail.com>

--- src/components/Functional/CarCard/index.jsx
+++ src/components/Functional/CarCard/index.jsx
@@ -6,21 +6,43 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 import { moneyFormat } from "helpers/numbers";
 
 export default function CarCard({ carData }) {
-  const { id, photos, name, price, description } = carData;
+  const { id, photos, name, price, description, status } = carData;
   return (
     <Grid item xs={12} sx={{ paddingBottom: "1px" }}>
-      <Card>
+      <Card sx={{ position: "relative", overflow: "hidden" }}>
         <CardActionArea>
           <CardMedia
             component="img"
             height="180"
             image={photos[0]}
             alt={name}
+            sx={{ objectFit: "cover" }}
           />
+          {status === "sold" && (
+            <Box
+              fullWidth
+              sx={{
+                fontSize: 40,
+                fontWeight: "bold",
+                position: "absolute",
+                color: "#FFF",
+                transform: `rotate(-20deg)`,
+                backgroundColor: "#484848",
+                width: "120%",
+                top: 60,
+                left: -18,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              VENDIDO
+            </Box>
+          )}
           <CardContent sx={{ height: 180 }}>
             <Typography
               sx={{
