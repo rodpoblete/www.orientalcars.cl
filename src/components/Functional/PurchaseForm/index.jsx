d521aca17832fb7d1d04153f526569a67992df96
Rodrigo Poblete <rorro.poblete@gmail.com>
2023-01-27 16:43:14 -0300

    chore: replace formcontrol with textfield component

--- src/components/Functional/PurchaseForm/index.jsx
+++ src/components/Functional/PurchaseForm/index.jsx
@@ -3,10 +3,7 @@ import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import InputAdornment from "@mui/material/InputAdornment";
-import { Button, FormHelperText, Paper } from "@mui/material";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Input from "@mui/material/Input";
+import { Button, Paper } from "@mui/material";
 
 export default function PurchaseForm() {
   const [fullName, setFullName] = useState("");
@@ -53,10 +50,10 @@ export default function PurchaseForm() {
               label="Rut"
               value={rut}
               variant="standard"
+              fullWidth
               inputProps={{
                 pattern: "^[0-9]+-[0-9kK]{1}$",
               }}
-              fullWidth
               onChange={(event) => setRut(event.target.value)}
             />
           </Grid>
@@ -67,26 +64,28 @@ export default function PurchaseForm() {
               label="Fecha de nacimiento"
               value={birth}
               variant="standard"
-              InputLabelProps={{ shrink: true }}
               fullWidth
+              InputLabelProps={{ shrink: true }}
               onChange={(event) => setBirth(event.target.value)}
             />
           </Grid>
           <Grid xs={12} sm={6} md={4} item>
-            <FormControl fullWidth variant="standard" required>
-              <InputLabel htmlFor="standard-adornment-phone">
-                Teléfono
-              </InputLabel>
-              <Input
-                value={phone}
-                onChange={(event) => setPhone(event.target.value)}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]{8}" }}
-                startAdornment={
+            <TextField
+              required
+              helperText="Ej: 8 dígitos"
+              label="Teléfono"
+              value={phone}
+              variant="standard"
+              fullWidth
+              inputProps={{
+                inputMode: "numeric",
+                pattern: "[0-9]{8}",
+                startAdornment: (
                   <InputAdornment position="start">+569</InputAdornment>
-                }
-              />
-              <FormHelperText>8 dígitos</FormHelperText>
-            </FormControl>
+                ),
+              }}
+              onChange={(event) => setPhone(event.target.value)}
+            />
           </Grid>
           <Grid xs={12} sm={6} md={4} item>
             <TextField
@@ -117,36 +116,40 @@ export default function PurchaseForm() {
             />
           </Grid>
           <Grid xs={12} sm={6} md={4} item>
-            <FormControl fullWidth variant="standard" required>
-              <InputLabel htmlFor="standard-adornment-salary">
-                Sueldo Líquido
-              </InputLabel>
-              <Input
-                value={salary}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-                onChange={(event) => setSalary(event.target.value)}
-                startAdornment={
+            <TextField
+              required
+              helperText="Ej: 900000 (sin puntos)"
+              label="Sueldo Líquido"
+              value={salary}
+              variant="standard"
+              fullWidth
+              inputProps={{
+                inputMode: "numeric",
+                pattern: "[0-9]*",
+                startAdornment: (
                   <InputAdornment position="start">$</InputAdornment>
-                }
-              />
-              <FormHelperText>Ej: 900000 (sin puntos)</FormHelperText>
-            </FormControl>
+                ),
+              }}
+              onChange={(event) => setSalary(event.target.value)}
+            />
           </Grid>
           <Grid xs={12} sm={6} md={4} item>
-            <FormControl fullWidth variant="standard" required>
-              <InputLabel htmlFor="standard-adornment-amount">
-                Monto del pie a abonar
-              </InputLabel>
-              <Input
-                value={amount}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-                onChange={(event) => setAmount(event.target.value)}
-                startAdornment={
+            <TextField
+              required
+              helperText="Ej: 2000000 (sin puntos)"
+              label="Monto del pie a abonar"
+              value={amount}
+              variant="standard"
+              fullWidth
+              inputProps={{
+                inputMode: "numeric",
+                pattern: "[0-9]*",
+                startAdornment: (
                   <InputAdornment position="start">$</InputAdornment>
-                }
-              />
-              <FormHelperText>Ej: 2000000 (sin puntos)</FormHelperText>
-            </FormControl>
+                ),
+              }}
+              onChange={(event) => setAmount(event.target.value)}
+            />
           </Grid>
           <Grid xs={12} align="center" mt={2} item>
             <Button
